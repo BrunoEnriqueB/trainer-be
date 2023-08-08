45e79335ce504f37bd30ace9391d2f8e661d81ee
Bruno Enrique <brunobaronenrique@gmail.com>
2023-08-07 22:14:15 -0300

    First commit

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import 'dotenv/config';
+
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+  return res.send('<h1>Hello World!</h1>');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
