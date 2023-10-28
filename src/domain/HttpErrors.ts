2924b66cc427b72d154a96ec2162da1acd2e376d
Bruno Enrique <brunobaronenrique@gmail.com>
2023-10-28 18:34:25 -0300

    add error handler

---
+++ src/domain/HttpErrors.ts
@@ -0,0 +1,36 @@
+export class HttpError<T> extends Error {
+  public statusCode: number;
+  public errors: T[];
+
+  constructor(statusCode: number, message?: string, errors?: T[]) {
+    super();
+
+    this.statusCode = statusCode;
+    this.message = message || 'Internal Server Error';
+    this.errors = errors || [];
+  }
+}
+
+export class BadRequestError<T> extends HttpError<T> {
+  constructor(errors?: T[]) {
+    super(400, 'Bad Request', errors);
+  }
+}
+
+export class NotFoundError<T> extends HttpError<T> {
+  constructor(errors?: T[]) {
+    super(404, 'Bad Request', errors);
+  }
+}
+
+export class UnauthorizedError<T> extends HttpError<T> {
+  constructor(errors?: T[]) {
+    super(401, 'Bad Request', errors);
+  }
+}
+
+export class InternalServerError<T> extends HttpError<T> {
+  constructor(errors?: T[]) {
+    super(500, 'Internal Server Error', errors);
+  }
+}
--- src/middlewares/ErrorHandler.ts
+++ src/middlewares/ErrorHandler.ts
@@ -1,15 +1,18 @@
+import { HttpError } from '@src/domain/HttpErrors';
 import { NextFunction, Request, Response } from 'express';
-import { HttpError } from '@/@types/HttpErrors';
+
 const NODE_ENV = process.env.NODE_ENV;
 
-export default function (
-  err: HttpError,
+export default function <T>(
+  err: HttpError<T>,
   req: Request,
   res: Response,
   next: NextFunction
 ) {
   res.status(err.statusCode).json({
+    success: false,
     message: err.message,
+    errors: err.errors,
     name: err.name,
     stack: NODE_ENV !== 'production' ? err.stack : {}
   });
