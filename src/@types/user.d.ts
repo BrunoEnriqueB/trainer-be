e89b411176ab39d50707c95b098bebff573b7f53
Bruno Enrique <brunobaronenrique@gmail.com>
2023-10-30 21:44:19 -0300

    add user types

---
+++ src/@types/user.d.ts
@@ -0,0 +1,12 @@
+export type publicUser = {
+  name: string;
+  document: string;
+  email: string;
+  created_at: Date;
+  updated_at: Date;
+};
+
+export type userData = {
+  document: string;
+  email: string;
+};
