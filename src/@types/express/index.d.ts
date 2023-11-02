3ebbd3dd70ebeca619f40033a7a2fae3dffaae41
Bruno Enrique <brunobaronenrique@gmail.com>
2023-11-02 19:37:48 -0300

    add user, trainer and student on express request object

---
+++ src/@types/express/index.d.ts
@@ -0,0 +1,25 @@
+export {};
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: {
+        name: string;
+        document: string;
+        email: string;
+        created_at: Date;
+        updated_at: Date;
+      };
+
+      trainer?: {
+        id: string;
+        user_id: string;
+      };
+
+      student?: {
+        id: string;
+        user_id: string;
+      };
+    }
+  }
+}
